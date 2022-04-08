81a6cac38736160d4bc43e0a68fe6506e3b83993
kaku8810 <59243400+kaku8810@users.noreply.github.com>
2022-04-09 08:50:28 +0900

    commit

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-n = int(input())
-count = [[[0 for i in range(10)] for j in range(3)] for k in range(4)]
-for l in range(n):
-    b, f, r, v = 3, 4, 5, 6
-    count[b][f][r] = v
--- 公舎の入居者数.py
+++ 公舎の入居者数.py
@@ -1,6 +1,11 @@
 n = int(input())
 count = [[[0 for i in range(10)] for j in range(3)] for k in range(4)]
 for l in range(n):
-    b, f, r, v = 3, 4, 5, 6
-    count[b][f][r] = v
-print(v)
+    b, f, r, v = map(int, input().split())
+    count[b-1][f-1][r-1] += v
+for building in range(4):
+    for floor in range(3):
+        for room in range(10):
+            print(count[building][floor][room], end=' ')
+        print()
+    print("#"*20)
