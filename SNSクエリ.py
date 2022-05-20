5ae189ba317401b76045f5a615f4c13ec45c4e6e
kaku8810 <59243400+kaku8810@users.noreply.github.com>
2022-05-21 08:54:00 +0900

    SNSクエリ正答

--- SNSクエリ.py
+++ SNSクエリ.py
@@ -6,8 +6,20 @@ User = [[] for i in range(N)]
 for i in range(Q):
     query = list(map(int, input().split()))
     query_type = query[0]
+
     # Followクエリの処理
     if query_type == 0:
-
+        x = query[1]
+        y = query[2]
+        User[x].append(y)
     # GetFollowersクエリの処理
     else:
+        followers = []
+        z = query[1]
+        for j in range(N):
+            if z in User[j]:
+                followers.append(j)
+        if followers:
+            print(*followers)
+        else:
+            print('No')
