ce02629237c4b18928bf4de0689e1ae072effd66
kaku8810 <59243400+kaku8810@users.noreply.github.com>
2022-06-04 10:38:21 +0900

    ごはんを買う

--- アルゴ式/ごはんを買う.py
+++ アルゴ式/ごはんを買う.py
@@ -1,7 +1,20 @@
 # 入力
 N,K = map(int, input().split())
+S = [tuple(map(int, input().split())) for _ in range(N)]
 
+# ソート
+S.sort()
 
-# Aでソート
+# K個になるまで繰り返し
+ans = 0
+for (A,B) in S:
+    # 買う個数
+    num = min(K, B)
 
-# 
+    # 買う
+    K -= num
+
+    # 金額を足す
+    ans += A * num
+
+print(ans)
