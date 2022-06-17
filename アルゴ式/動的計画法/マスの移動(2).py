8f381c62b9a5fce5a611a4892f5eaf86cfe2fdec
kaku8810 <59243400+kaku8810@users.noreply.github.com>
2022-06-17 09:00:05 +0900

    マスの移動

---
+++ アルゴ式/動的計画法/マスの移動(2).py
@@ -0,0 +1,14 @@
+# 入力
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+
+# 初期値
+INF = 1000000
+T = [INF] * N
+T[0] = 0
+
+# 計算
+for i in range(1,N):
+    for j in range()
+
+# 出力
