fb6800dcb3161f4649b1eca9461675c5a81e2e0c
kaku8810 <59243400+kaku8810@users.noreply.github.com>
2022-06-18 08:38:13 +0900

    マスの移動

--- アルゴ式/動的計画法/マスの移動(2).py
+++ アルゴ式/動的計画法/マスの移動(2).py
@@ -8,7 +8,10 @@ T = [INF] * N
 T[0] = 0
 
 # 計算
-for i in range(1,N):
-    for j in range()
+for i in range(1, N):
+    for j in range(1, M + 1):
+        if i - j >= 0:
+            T[i] = min(T[i], T[i - j] + A[i] * j)
 
 # 出力
+print(T[-1])
