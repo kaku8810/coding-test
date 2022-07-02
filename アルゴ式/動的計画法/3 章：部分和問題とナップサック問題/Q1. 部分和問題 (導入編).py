69c143df7d4c507c8e14f0a04bb28943b258cfa2
kaku8810 <59243400+kaku8810@users.noreply.github.com>
2022-07-03 08:57:30 +0900

    部分和問題導入

--- アルゴ式/動的計画法/3 章：部分和問題とナップサック問題/Q1. 部分和問題 (導入編).py
+++ アルゴ式/動的計画法/3 章：部分和問題とナップサック問題/Q1. 部分和問題 (導入編).py
@@ -2,15 +2,20 @@
 N, M = map(int, input().split())
 A = list(map(int, input().split()))
 
-# 計算
+# dp用意
 dp = [[False] * M for _ in range(N)]
 dp[0][0] = True
-for i in range(N):
+
+# 順に計算
+for i in range(N - 1):
     for j in range(M):
-        # 1つ上のマスがTrueならTrueにする
-        if i - 1 >= 0 and dp[i - 1][j]:
-            dp[i][j] = True
-        # 1つ上、A[i]つ左のマスがTrueならTrueにする
-        if
+        if not dp[i][j]:
+            continue
+        # 真下のマスにコマを渡す
+        dp[i + 1][j] = True
+        # 右下のマスにコマを渡す
+        if j + A[i] < M:
+            dp[i + 1][j + A[i]] = True
 
 # 出力
+print(sum(dp[-1]))
