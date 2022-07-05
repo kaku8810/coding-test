87726639d26d128139fb75603005fc03e537771f
kaku8810 <59243400+kaku8810@users.noreply.github.com>
2022-07-06 08:53:13 +0900

    部分和問題

--- アルゴ式/動的計画法/3 章：部分和問題とナップサック問題/Q2. 部分和問題.py
+++ アルゴ式/動的計画法/3 章：部分和問題とナップサック問題/Q2. 部分和問題.py
@@ -1,4 +1,22 @@
 # 入力
+N, M = map(int, input().split())
+W = list(map(int, input().split()))
+
 # dp用意
+dp = [[False] * (M + 1) for _ in range(N + 1)]
+dp[0][0] = True
+
 # 順に計算
+for i in range(N):
+    for j in range(M + 1):
+        if not dp[i][j]:
+            continue
+        dp[i + 1][j] = True
+        if j + W[i] <= M:
+            dp[i + 1][j + W[i]] = True
+
 # 出力
+if dp[-1][-1]:
+    print("Yes")
+else:
+    print("No")
