03e2f02e99dac5800d2f6f9fb1aa87269fc4f41d
kaku8810 <59243400+kaku8810@users.noreply.github.com>
2022-07-16 17:58:11 +0900

    最長の共通接頭辞の正答

--- LeetCode/14. Longest Common Prefix.py
+++ LeetCode/14. Longest Common Prefix.py
@@ -1,4 +1,12 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-       # 接頭辞を返却する
-       
+        # 最小の文字列を取得
+        shortest = min(strs, key=len)
+        # 最小の文字列を基準に比較
+        for i, ch in enumerate(shortest):
+            for other in strs:
+                if other[i] != ch:
+                    # 比較した文字が違う場合は前の文字までを返す
+                    return shortest[:i]
+        # 最後まで一致したらそのまま帰す
+        return shortest
