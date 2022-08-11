a2bf9badecfa84088bb53dd8627a441d11381e5b
kaku8810 <59243400+kaku8810@users.noreply.github.com>
2022-08-11 09:10:25 +0900

    素数判定

---
+++ アルゴ式/グラフアルゴリズム/整数論的アルゴリズム/1 章：素数判定/Q2. 素数判定 (弱).py
@@ -0,0 +1,18 @@
+# 入力
+N = int(input())
+
+# 素数かどうかを判定する関数
+def is_prime(n):
+    if n == 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+
+# 出力
+if is_prime(N):
+    print("Yes")
+else:
+    print("No")
