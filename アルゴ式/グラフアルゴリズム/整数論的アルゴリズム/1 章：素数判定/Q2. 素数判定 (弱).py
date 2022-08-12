a47455e438c389bed06a352286297831a712c792
kaku8810 <59243400+kaku8810@users.noreply.github.com>
2022-08-13 08:43:42 +0900

    約数の個数

--- アルゴ式/グラフアルゴリズム/整数論的アルゴリズム/1 章：素数判定/Q2. 素数判定 (弱).py
+++ アルゴ式/グラフアルゴリズム/整数論的アルゴリズム/1 章：素数判定/Q2. 素数判定 (弱).py
@@ -1,6 +1,3 @@
-# 入力
-N = int(input())
-
 # 素数かどうかを判定する関数
 def is_prime(n):
     if n == 1:
@@ -10,6 +7,9 @@ def is_prime(n):
             return False
     return True
 
+
+# 入力
+N = int(input())
 
 # 出力
 if is_prime(N):
--- アルゴ式/グラフアルゴリズム/整数論的アルゴリズム/1 章：素数判定/Q3. 素数判定 (強).py
+++ アルゴ式/グラフアルゴリズム/整数論的アルゴリズム/1 章：素数判定/Q3. 素数判定 (強).py
@@ -1,6 +1,3 @@
-# 入力
-N = int(input())
-
 # 素数かどうかを判定する関数
 def is_prime(n):
     if n == 1:
@@ -12,6 +9,9 @@ def is_prime(n):
         i += 1
     return True
 
+
+# 入力
+N = int(input())
 
 # 出力
 if is_prime(N):
