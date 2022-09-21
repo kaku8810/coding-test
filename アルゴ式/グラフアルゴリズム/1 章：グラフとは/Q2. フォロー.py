a49059e8735d30d41bb5773a070aa8968ee59d55
kaku8810 <59243400+kaku8810@users.noreply.github.com>
2022-09-21 09:57:47 +0900

    フォロー

--- アルゴ式/グラフアルゴリズム/1 章：グラフとは/Q2. フォロー.py
+++ アルゴ式/グラフアルゴリズム/1 章：グラフとは/Q2. フォロー.py
@@ -1,13 +1,11 @@
-# 入力
 N, M = map(int, input().split())
 
-# フォロー関係を記録
 G = [[] for _ in range(N)]
+
 for i in range(M):
     A, B = map(int, input().split())
     G[A].append(B)
 
-# 出力
 for i in range(N):
     G[i].sort()
-    print(" ".join(map(str, G[i])))
+    print(*G[i])
