4b52e15b9f05b8d1a83b8bbf518b53d2d68f3f91
kaku8810 <59243400+kaku8810@users.noreply.github.com>
2023-01-27 11:41:19 +0900

    連結リスト更新

--- アルゴ式/さまざまなデータ構造/4 章：連結リスト/Q1. 連結リスト.py
+++ アルゴ式/さまざまなデータ構造/4 章：連結リスト/Q1. 連結リスト.py
@@ -1,5 +1,29 @@
-Q = int(input())
+# 連結リストの各ノード
+class Node:
+    def __init__(self, value=""):
+        self.next = None  # 次がどのノードを指すか
+        self.value = value  # ノードの値
 
-for i in range(Q):
-    a, b = input().split()
-    if 
+
+# ノードの初期化
+nil = Node()
+nil.next = nil
+
+
+# 連結リストの先頭に要素を追加
+def insert(v):
+    v.next = nil.next  # vの次を現在の先頭にする
+    nil.next = v  # 先頭をvにする
+
+
+# 入力
+Q = int(input())
+for query in range(Q):
+    type, S = input().split()
+    if type == "0":
+        # ノードを作成する
+        v = Node(S)
+        insert(v)
+    else:
+        # 先頭からk個(=S)を出力する
+        
