91ef28d32e9f12bd201d7605f290e1b53bc4c5db
kaku8810 <59243400+kaku8810@users.noreply.github.com>
2023-02-11 10:13:32 +0900

    地図の切り取り

--- アルゴ式/さまざまなデータ構造/3 章：二次元配列/Q3. 地図の切り取り.py
+++ アルゴ式/さまざまなデータ構造/3 章：二次元配列/Q3. 地図の切り取り.py
@@ -1,7 +1,17 @@
+# 入力
 H, W, x, y = map(int, input().split())
 S = [input() for _ in range(H)]
 
+# 出力
 for i in range(x - 1, x + 2):
     for j in range(y - 1, y + 2):
         print(S[i][j], end="")
     print()
+
+"""スライスを使うともっと簡単に書ける
+H, W, x, y = list(map(int, input().split()))
+S = [input() for _ in range(H)]
+
+for i in range(x - 1, x + 2):
+    print(S[i][y - 1 : y + 2])
+"""
